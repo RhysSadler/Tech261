076aa5aeadd9c5ac657cdd6062d8144316dea67d
Rhys Sadler <Rhys.sadler.rs@Gmail.com>
2024-06-17 16:02:30 +0100

    changes

--- JavaBasics/src/test/java/com/sparta/rs/ProgramTest.java
+++ JavaBasics/src/test/java/com/sparta/rs/ProgramTest.java
@@ -1,12 +1,26 @@
 package com.sparta.rs;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ProgramTest {
-
     @Test
-    public void basicTest() {
-        Assertions.assertTrue(5==4);
+    @DisplayName("given a time of 21, then the greeting should be Good Evening")
+    void checkThat21GivesGoodEvening(){
+        int time = 21;
+        String exspectedGreeting = "Good evening!";
+        String actualGreeting = Program.getGreeting(time);
+        Assertions.assertEquals(exspectedGreeting, actualGreeting);
     }
+    @Test
+    @DisplayName("given a time of 14, then the greeting should be Good afternoon!")
+    void checkThat11GivesGoodAfternoon(){
+        int time = 14;
+        String exspectedGreeding = "Good afternoon!";
+        String actualGreeting = Program.getGreeting(time);
+        Assertions.assertEquals(exspectedGreeding,actualGreeting);
+    }
+
 }
+
