d661933a2a64cac2850f64e2469855af51372487
Rhys Sadler <Rhys.sadler.rs@Gmail.com>
2024-06-19 16:20:13 +0100

    added the coding kata

---
+++ CodingKata/src/main/java/com/sparta/rs/coding_Kata/App.java
@@ -0,0 +1,9 @@
+package com.sparta.rs.coding_Kata;
+
+public class App {
+    public static void main(String[] args) {
+        int[] array = new int[]{1,2,3,3,3,4};
+        System.out.println(codingKata.slidingWindow(array));
+
+    }
+}
---
+++ CodingKata/src/main/java/com/sparta/rs/coding_Kata/codingKata.java
@@ -0,0 +1,16 @@
+package com.sparta.rs.coding_Kata;
+
+public class codingKata {
+    public static boolean slidingWindow(int[] array) {
+        if (array == null|| array.length < 3) {
+            return false;
+        }
+        for (int i = 0; i <= array.length; i++) {
+            if ((array[i] == array[i+1]) && (array[i] == array[i+2])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
